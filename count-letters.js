9a7a4bdf40611dd4ed3a7e4233069f31e81bcc99
Zushisan <74838@supinfo.com>
2018-01-17 00:27:00 +0000

    counting letters working

--- count-letters.js
+++ count-letters.js
@@ -8,9 +8,49 @@
 
 function countLetters(string){
 
-  var letters = string.split("");
+  var letters = string.split(" ").join("").toLowerCase();
 
-  console.log(letters);
+  // output is an empty object where we will store our letters
+  var output = {};
+
+  // We want to store our letters in the object ONLY if they arnt in the object yet
+
+  for ( var letter of letters){
+
+    if (letter.match(/[a-z]/i)){
+
+      if (output[letter]){
+
+        output[letter] += 1;
+
+      }
+
+      else {
+        output[letter] = 1;
+      }
+    }
+  }
+
+  // for ( var i = 0; i < letters.length; i++){
+
+  //   if (letters[i].match(/[a-z]/i)){
+
+  //     if (output[letters[i]]){
+
+  //       output[letters[i]] += 1;
+
+  //     }
+
+  //     else {
+
+  //       output[letters[i]] = 1;
+
+  //     }
+  //   }
+  // }
+
+return output;
+
 }
 
-countLetters("My name is Romain.");
+console.log(countLetters("My name is Romain."));
