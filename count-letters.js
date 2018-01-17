41658c6c34435b2805b52ca1797c83b052116829
Zushisan <74838@supinfo.com>
2018-01-17 00:02:25 +0000

    Split our string

---
+++ count-letters.js
@@ -0,0 +1,16 @@
+// Write a function countLetters that return all unique characters that exist in a string that is passed into the function.
+
+// Also return how many instances of each letter found in th string
+
+// countLetters needs to return an object
+
+
+
+function countLetters(string){
+
+  var letters = string.split("");
+
+  console.log(letters);
+}
+
+countLetters("My name is Romain.");
